94b8c7a5d48c3485b8f9fae51b99f3c61a254592
zhzane <zhzane@outlook.com>
2019-05-14 10:54:05 +0800

    update buffer API

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -1,3 +1,18 @@
+/**
+ * @file buffer.h
+ * @author zhzane (zhzane@outlook.com)
+ * @brief 内存分配接口
+ * @version 0.1
+ * @date 2019-05-14
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ * - BufferPtr: 内存区域指针接口，由具体实现继承，需要在析构函数中处理内存回收
+ * - Buffer: 共享指针的封装，指向 BufferPtr
+ * - BufferList: Buffer列表的封装实现
+ * - BufferAllocator: Buffer分配器接口，所有内存分配器都需要继承该接口
+ * 
+ */
 #ifndef FLAME_INCLUDE_BUFFER_H
 #define FLAME_INCLUDE_BUFFER_H
 
@@ -6,19 +21,46 @@
 #include <list>
 #include <memory>
 
+enum BufferTypes {
+    BUFF_TYPE_NORMAL    = 0,
+    BUFF_TYPE_DMA       = 0x1,
+    BUFF_TYPE_RDMA      = 0x2
+};
+
 namespace flame {
 
 class BufferPtr {
 public:
     virtual ~BufferPtr() {}
 
-    virtual uint8_t* addr() const = 0;
+    /**
+     * @brief Get Address of Buffer
+     * 
+     * @return void* 
+     */
+    virtual void* addr() const = 0;
 
+    /**
+     * @brief Get Size of Buffer
+     * 
+     * @return size_t 
+     */
     virtual size_t size() const = 0;
 
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
     virtual bool null() const { return addr() == nullptr || size() == 0; }
 
-    virtual uint8_t* end() const { return addr() + size(); }
+    /**
+     * @brief Get the Tail of Buffer
+     * 
+     * @return void* 
+     */
+    virtual void* end() const { return addr() + size(); }
 
     /**
      * @brief Buffer类型
@@ -29,7 +71,7 @@ public:
 
     /**
      * @brief 按指定大小调整Buffer空间
-     * 默认拒绝重新分配
+     * 默认拒绝重新分配，可以不实现调整功能
      * @param sz 新大小
      * @return true 调整成功
      * @return false 调整失败
@@ -73,23 +115,88 @@ public:
     Buffer() {}
     Buffer(const std::shared_ptr<BufferPtr>& ptr) : ptr_(ptr) {}
 
-    void set(const std::shared_ptr<BufferPtr>& ptr) { ptr_ = ptr; }
+    /**
+     * @brief 
+     * 
+     * @param ptr 
+     */
+    inline void set(const std::shared_ptr<BufferPtr>& ptr) { ptr_ = ptr; }
 
-    bool null() const { return ptr_.get() == nullptr || ptr_->null(); }
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
+    inline bool null() const { return ptr_.get() == nullptr || ptr_->null(); }
 
-    bool valid() const { return !null(); }
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
+    inline bool valid() const { return ! null(); }
 
-    uint8_t* addr() const { return ptr_->addr(); }
+    /**
+     * @brief 
+     * 
+     * @return uint8_t* 
+     */
+    inline void* addr() const { return ptr_->addr(); }
 
-    size_t size() const { return ptr_->size(); }
+    /**
+     * @brief 
+     * 
+     * @return size_t 
+     */
+    inline size_t size() const { return ptr_->size(); }
 
-    uint8_t* end() const { return ptr_->end(); }
+    /**
+     * @brief  
+     * 
+     * @return uint8_t* 
+     */
+    inline void* end() const { return ptr_->end(); }
 
-    int type() const { return ptr_->type(); }
+    /**
+     * @brief 
+     * 
+     * @return int 
+     */
+    inline int type() const { return ptr_->type(); }
 
-    bool resize(size_t sz) { return ptr_->resize(sz); }
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
+    inline bool is_normal() const { return ptr_->type() == BUFF_TYPE_NORMAL; }
+    inline bool is_dma() const { return ptr_->type() == BUFF_TYPE_DMA; }
+    inline bool is_rdma() const { return ptr_->type() == BUFF_TYPE_RDMA; }
 
-    BufferPtr* get() { return ptr_.get(); }
+    /**
+     * @brief 
+     * 
+     * @param sz 
+     * @return true 
+     * @return false 
+     */
+    inline bool resize(size_t sz) { return ptr_->resize(sz); }
+
+    /**
+     * @brief 
+     * 
+     * @return BufferPtr* 
+     */
+    inline BufferPtr* get() const { return ptr_.get(); }
+
+    /**
+     * @brief 清楚引用
+     * 
+     */
+    inline void clear() { ptr_.reset(nullptr); }
 
     Buffer(const Buffer&) = default;
     Buffer(Buffer&&) = default;
@@ -110,28 +217,67 @@ public:
      * 
      * @return size_t 
      */
-    size_t size() const { return bsize_; }
+    inline size_t size() const { return bsize_; }
 
     /**
      * @brief Buffer分段数量
      * 
      * @return size_t 
      */
-    size_t count() const { return blist_.size(); }
+    inline size_t count() const { return blist_.size(); }
 
-    bool empty() const { return count() == 0 || size() == 0; }
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
+    inline bool empty() const { return count() == 0 || size() == 0; }
 
-    std::list<Buffer>::const_iterator begin() const { return blist_.cbegin(); }
-    std::list<Buffer>::const_iterator end() const { return blist_.cend(); }
-    std::list<Buffer>::const_reverse_iterator rbegin() const { return blist_.crbegin(); }
-    std::list<Buffer>::const_reverse_iterator rend() const { return blist_.crend(); }
+    /**
+     * @brief 头部迭代器
+     * 
+     * @return std::list<Buffer>::const_iterator 
+     */
+    inline std::list<Buffer>::const_iterator begin() const { return blist_.cbegin(); }
 
-    void push_front(const Buffer& buff) {
+    /**
+     * @brief 尾部迭代器
+     * 
+     * @return std::list<Buffer>::const_iterator 
+     */
+    inline std::list<Buffer>::const_iterator end() const { return blist_.cend(); }
+    
+    /**
+     * @brief 反向-头部迭代器
+     * 
+     * @return std::list<Buffer>::const_reverse_iterator 
+     */
+    inline std::list<Buffer>::const_reverse_iterator rbegin() const { return blist_.crbegin(); }
+    
+    /**
+     * @brief 反向-尾部迭代器
+     * 
+     * @return std::list<Buffer>::const_reverse_iterator 
+     */
+    inline std::list<Buffer>::const_reverse_iterator rend() const { return blist_.crend(); }
+
+    /**
+     * @brief 追加到头部
+     * 
+     * @param buff 
+     */
+    inline void push_front(const Buffer& buff) {
         blist_.push_front(buff);
         bsize_ += buff.size();
     }
 
-    void push_front(const BufferList& bl) {
+    /**
+     * @brief 追加到头部
+     * 
+     * @param bl 
+     */
+    inline void push_front(const BufferList& bl) {
         if (&bl == this || bl.empty()) 
             return;
         for (auto it = bl.rbegin(); it != bl.rend(); it++)
@@ -139,19 +285,33 @@ public:
         bsize_ += bl.size();
     }
 
-    void pop_front() {
+    /**
+     * @brief 弹出头部
+     * 
+     */
+    inline void pop_front() {
         if (blist_.empty())
             return;
         bsize_ -= blist_.front().size();
         blist_.pop_front();
     }
 
-    void push_back(const Buffer& buff) {
+    /**
+     * @brief 追加到尾部
+     * 
+     * @param buff 
+     */
+    inline void push_back(const Buffer& buff) {
         blist_.push_back(buff);
         bsize_ += buff.size();
     }
 
-    void push_back(const BufferList& bl) {
+    /**
+     * @brief 追加到尾部
+     * 
+     * @param bl 
+     */
+    inline void push_back(const BufferList& bl) {
         if (&bl == this || bl.empty())
             return;
         for (auto it = bl.begin(); it != bl.end(); it++)
@@ -159,7 +319,11 @@ public:
         bsize_ += bl.size();
     }
 
-    void pop_back() {
+    /**
+     * @brief 弹出尾部
+     * 
+     */
+    inline void pop_back() {
         if (blist_.empty())
             return;
         bsize_ -= blist_.back().size();
@@ -188,16 +352,56 @@ public:
     virtual ~BufferAllocator() {}
 
     /**
-     * @brief Buffer分配器所能分配的最大大小（不是Free空间）
+     * @brief 所分配的内存类型
+     * 
+     * @return int 
+     */
+    virtual int type() const = 0;
+
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
+    inline bool is_normal() const { return this->type() == BUFF_TYPE_NORMAL; }
+    inline bool is_dma() const { return this->type() == BUFF_TYPE_DMA; }
+    inline bool is_rdma() const { return this->type() == BUFF_TYPE_RDMA; }
+
+    /**
+     * @brief 所能分配的最大内存（不是Free空间）
      * 
      * @return size_t 
      */
     virtual size_t max_size() const = 0;
 
     /**
+     * @brief 所能分配的最小内存
+     * 
+     * @return size_t 
+     */
+    virtual size_t min_size() const = 0;
+
+    /**
+     * @brief 剩余空间
+     * 
+     * @return size_t 当有剩余空间，但无法准确获取时，返回BUFF_ALLOC_FULL_SIZE
+     */
+    virtual size_t free_size() const = 0;
+#define BUFF_ALLOC_FULL_SIZE    (~0ULL)
+
+    /**
+     * @brief 为空
+     * 
+     * @return true 没有剩余空间
+     * @return false 
+     */
+    virtual bool empty() const = 0;
+
+    /**
      * @brief 分配指定大小的Buffer
      * 需要检查Buffer是否有效(valid)，
-     * 当Buffer的null()返回true或valid()返回false时，
+     * 当Buffer的is_null()返回true或valid()返回false时，
      * 表示内存分配器不能够再分配指定大小的内存
      * @param sz 
      * @return Buffer 
