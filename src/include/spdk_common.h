bb830ecfa6ec32d7b97475d5d24e98262b56c81e
chenxuqiang <cxq507@foxmail.com>
2019-05-16 16:05:00 +0800

    添加一个spdk_common.h, 包含了spdk相关头文件

---
+++ src/include/spdk_common.h
@@ -0,0 +1,13 @@
+#ifndef FLAME_INCLUDE_SPDK_COMMON_H
+#define FLAME_INCLUDE_SPDK_COMMON_H
+
+#include "spdk/bdev.h"
+#include "spdk/event.h"
+#include "spdk/env.h"
+#include "spdk/blob_bdev.h"
+#include "spdk/blob.h"
+#include "spdk/log.h"
+#include "spdk/version.h"
+#include "spdk/string.h"
+
+#endif
